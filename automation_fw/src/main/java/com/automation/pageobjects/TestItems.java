e84768ff578aa389c395e765a2538e3aa5e12d8f
Kiss <robert.kiss@siemens.com>
2020-01-21 14:52:25 +0200

    done first test and added first steps for other 2 tests

--- automation_fw/src/main/java/com/automation/pageobjects/TestItems.java
+++ automation_fw/src/main/java/com/automation/pageobjects/TestItems.java
@@ -1,16 +1,15 @@
 package com.automation.pageobjects;
 
 import com.automation.TestBase;
-import com.automation.helpers.Configuration;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Formatter;
+import java.util.List;
 
 
 public class TestItems extends TestBase {
@@ -36,6 +35,30 @@ public class TestItems extends TestBase {
 
     @FindBy(xpath = "//*[@aria-label = 'Move forward to switch to the next month.']")
     private WebElement nextMonthButtonCheckOut;
+
+    @FindBy(xpath = "//*[@id='lp-guestpicker']")
+    private WebElement guestsField;
+
+    @FindBy(xpath = "//*[@id='lp-guestpicker' and @aria-expanded='true']")
+    private WebElement guestsFieldExpanded;
+
+    @FindBy(xpath = "//*/div[contains(@aria-labelledby, 'adults')]/div/div[2]/div/div[3]/button")
+    private WebElement addAdult;
+
+    @FindBy(xpath = "//*/div[contains(@aria-labelledby, 'children')]/div/div[2]/div/div[3]/button")
+    private WebElement addChild;
+
+    @FindBy (xpath = "//*/button[@type = 'submit']/span[contains(text(), 'Search')]")
+    private WebElement homePageSearch;
+
+    @FindBy (xpath = "//*[@aria-label = 'Search']")
+    private WebElement searchField;
+
+    @FindBy (xpath = "//*[@id=\"menuItemButton-guest_picker\"]/button/span[2]/span")
+    private WebElement guestNumberAfterSearch;
+
+    @FindBy (xpath = "//*[@id=\"menuItemButton-date_picker\"]/button/span[2]/span")
+    private WebElement dateIntervalAfterSearch;
 
     public void setWhereField(String where){
         waitForElementToBeVisible(whereField);
@@ -69,8 +92,6 @@ public class TestItems extends TestBase {
     }
 
     public void setCheckInDate(int days){
-        //waitForElementToBeVisible(calendar, FIVE_SEC_WAIT);
-        //WebElement month = driver.findElement(By.xpath("//*/strong[contains(text() , '"+ getMonthLater(days) +"')]"));
         if(getMonth().equals(getMonthLater(days)) == false){
             nexMonthButtonCheckIn.click();
         }
@@ -80,13 +101,100 @@ public class TestItems extends TestBase {
     }
 
     public void setCheckOutDate(int days){
-        //waitForElementToBeVisible(calendar, FIVE_SEC_WAIT);
-        //WebElement month = driver.findElement(By.xpath("//*/strong[contains(text() , '"+ getMonthLater(days) +"')]"));
         if(getMonth().equals(getMonthLater(days)) == false){
             nextMonthButtonCheckOut.click();
         }
         WebElement day = driver.findElement(By.xpath("//*/td[contains(@aria-label,'"+ getMonthLater(days) +"') and contains(@aria-label, '" + getDayLater(days) + "')]"));
         waitForElementToBeClickable(day);
         day.click();
+    }
+
+    public void addGuestsField(){
+        click(guestsField);
+    }
+
+    public void addGuests(int guestNumber, WebElement guestType) throws InterruptedException {
+        for(int i = 0; i < guestNumber; i++){
+            click(guestType);
+            Thread.sleep(1000);
+        }
+    }
+
+    public void setAddAdult(int adultNumber) throws InterruptedException {
+        addGuests(adultNumber, addAdult);
+    }
+
+    public void setAddChild(int childNumber) throws InterruptedException {
+        addGuests(childNumber, addChild);
+    }
+
+    public void submitHomePage(){
+        click(homePageSearch);
+    }
+
+    public void closeGuestsField(){
+        click(guestsFieldExpanded);
+    }
+
+    public String getSearchFieldValue(){
+        String searchFieldValue = searchField.getAttribute("value");
+        return searchFieldValue;
+    }
+
+    public boolean isSearchQueryTheSame(String city) throws IOException, InterruptedException {
+        Thread.sleep(3000);
+        return checkPointBoolCondition(getSearchFieldValue().contains(city), ""+city+" is correct. ", ""+city+" is incorrect. ");
+    }
+
+    public String getGuestNumber(){
+        String guestNumber = guestNumberAfterSearch.getAttribute("aria-label");
+        return guestNumber;
+    }
+
+    public boolean isGuestNumberTheSame(int guestNumber) throws InterruptedException, IOException {
+        Thread.sleep(3000);
+        String guestNumberString = String.valueOf(guestNumber);
+        return checkPointBoolCondition(getGuestNumber().contains(guestNumberString), ""+guestNumberString+" is the amount of guests.", ""+guestNumberString+" is not the amount of guests.");
+    }
+
+    public String getDateInterval(){
+        String dateInterval = dateIntervalAfterSearch.getAttribute("aria-label");
+        return dateInterval;
+    }
+
+    public boolean isDateIntervalTheSame(int checkin, int checkout) throws InterruptedException, IOException {
+        Thread.sleep(3000);
+
+        String checkinDayString = String.valueOf(getDayLater(checkin));
+        String checkoutDayString = String.valueOf(getDayLater(checkout));
+        String checkinMonthShort = getMonthLater(checkin).substring(0, 3);
+        String checkoutMonthShort = getMonthLater(checkout).substring(0, 3);
+        String dateIntervalShort = ""+checkinMonthShort+" "+checkinDayString+" - "+checkoutMonthShort+" "+checkoutDayString+"";
+
+        return checkPointBoolCondition(getDateInterval().contains(dateIntervalShort), ""+dateIntervalShort+" is the correct interval.", ""+dateIntervalShort+" is not the correct interval.");
+    }
+
+    public boolean areEnoughApartmentGuests(int guests) throws IOException {
+        List<WebElement> propertyDetailsListElements = driver.findElements(By.xpath("//*/div[*]/div[1]/div[1]/div[1]/div[*]/a[@data-check-info-section = \"true\"]/../div[2]/div[3]"));
+        List<String> propertyDetailsList = new ArrayList<>();
+        List<Integer> numberOfGuestsList =  new ArrayList<>();
+        int i = 0;
+
+        for(WebElement propertyDetailsElement : propertyDetailsListElements){
+            propertyDetailsList.add(propertyDetailsElement.getText());
+        }
+
+        for(String accommodations : propertyDetailsList){
+            numberOfGuestsList.add(Integer.parseInt(accommodations.substring(0,1)));
+        }
+
+        for(int guestsInList : numberOfGuestsList){
+            if (guestsInList >= guests){
+                i = 1;
+            }else{
+                i = 0;
+            }
+        }
+        return checkPointBoolCondition(i == 1, ""+guests+" or more can be accommodated.", "Guest that can be accommodated is wrong.");
     }
 }
--- automation_fw/src/test/java/com/automation/tests/TestTests.java
+++ automation_fw/src/test/java/com/automation/tests/TestTests.java
@@ -2,15 +2,19 @@ package com.automation.tests;
 
 import com.automation.TestBase;
 import com.automation.pageobjects.TestItems;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 
 public class TestTests extends TestBase {
 
     TestItems testItems;
+    private SoftAssert softAssert = new SoftAssert();
+
 
     @BeforeMethod
     public void initializePages(){
@@ -18,14 +22,67 @@ public class TestTests extends TestBase {
     }
 
     @Test
-    public void verifyResultsBasedOnSearcCriteria(){
+    public void verifyResultsBasedOnSearchCriteria() throws InterruptedException, IOException {
         int daysCheckIn = 7;
         int daysCheckOut = 14;
+        String city = "Rome";
+        String country = "Italy";
+        String location = city +", " + country;
+        int adults = 2;
+        int children = 1;
 
-        testItems.setWhereField("Rome, Italy");
+        testItems.setWhereField(location);
         testItems.setCheckInDate(daysCheckIn);
         testItems.setCheckOutDate(daysCheckOut);
-        System.out.println("fdjk");
+        testItems.addGuestsField();
+        testItems.setAddAdult(adults);
+        testItems.setAddChild(children);
+        testItems.closeGuestsField();
+        testItems.submitHomePage();
+        softAssert.assertTrue(testItems.isSearchQueryTheSame(city), "City name.");
+        softAssert.assertTrue(testItems.isGuestNumberTheSame(adults + children), "Amount of guests.");
+        softAssert.assertTrue(testItems.isDateIntervalTheSame(daysCheckIn, daysCheckOut), "Date interval.");
+        softAssert.assertTrue(testItems.areEnoughApartmentGuests(adults + children), "Guests per apartment. ");
+        softAssert.assertAll();
+    }
 
+    @Test
+    public void verifyResultsAndDetailsMatchExtraFilters() throws InterruptedException {
+        int daysCheckIn = 7;
+        int daysCheckOut = 14;
+        String city = "Rome";
+        String country = "Italy";
+        String location = city +", " + country;
+        int adults = 2;
+        int children = 1;
+
+        testItems.setWhereField(location);
+        testItems.setCheckInDate(daysCheckIn);
+        testItems.setCheckOutDate(daysCheckOut);
+        testItems.addGuestsField();
+        testItems.setAddAdult(adults);
+        testItems.setAddChild(children);
+        testItems.closeGuestsField();
+        testItems.submitHomePage();
+    }
+
+    @Test
+    public void verifyPropertyIsDisplayedOnMapCorectly() throws InterruptedException {
+        int daysCheckIn = 7;
+        int daysCheckOut = 14;
+        String city = "Rome";
+        String country = "Italy";
+        String location = city +", " + country;
+        int adults = 2;
+        int children = 1;
+
+        testItems.setWhereField(location);
+        testItems.setCheckInDate(daysCheckIn);
+        testItems.setCheckOutDate(daysCheckOut);
+        testItems.addGuestsField();
+        testItems.setAddAdult(adults);
+        testItems.setAddChild(children);
+        testItems.closeGuestsField();
+        testItems.submitHomePage();
     }
 }
